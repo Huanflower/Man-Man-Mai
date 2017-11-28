110cbba9829eaf12981648dca8b7db04f1572003
zhaohuan <1987287033@qq.com>
2017-11-28 17:39:06 +0800

    productlist

--- mma/js/category.js
+++ mma/js/category.js
@@ -1,37 +1,57 @@
-$(function () {
-    $(".category_ul").on("click", ">li>a", function () {
-
+$(function(){
+    $("#categroy_title").on("click",".category_ul>li>a",function(){
+        // console.log($(this))  a
         $(this).siblings("ul").toggle();
+        //  $(this).parent()  li
+        //   $(this).parent().siblings("li")  兄弟元素li
+        //  $(this).parent().siblings("li").find("ul")   兄弟元素li的ul
         $(this).parent().siblings("li").find("ul").slideUp();
+        //console.log( $(this).attr("data_title_id"));
         var titleid = $(this).attr("data_title_id");
         var $that = $(this);
-        // console.log(titleid)
-        getcategory(titleid,$that)
-    })
-    getCategoryTitle();
-})
+        getcategoryMenu(titleid,$that)
 
-function getCategoryTitle() {
+    })
+
+
+    getcategoryTitle();
+
+})
+function getcategoryTitle(){
     $.ajax({
-        url: url + "api/getcategorytitle",
-        success:function(data){
-            // console.log(data);
-            var html = template("categoryTpl",data);
-            $(".category_ul").html(html);
+        url:url+"api/getcategorytitle",
+        success:function( data ){
+            // console.log(data)
+            var html = template("categoryTtileTpl",data);
+            $(".category_ul").html(html)
         }
     })
 }
 
 
-function getcategory(titleid,that){
-    $.ajax({
+function getcategoryMenu(titleid,$that){
+    if($that.siblings("ul").find("li").length==0){
+
+         $.ajax({
         url:url+"api/getcategory?titleid="+titleid,
         success:function(data){
             // console.log(data);
-            var html = template("categoryTpls",data);
-           var $ul = that.siblings("ul");
-           $ul.html(html);
-
+            var html = template("categoryMenuTpl",data);
+            var $ul = $that.siblings("ul");
+            $ul.html(html);
+            var lastlis = $ul.children().length%3 ||3 ;
+            // console.log(lastlis)
+            $ul.children("li:nth-last-child(-n"+lastlis+")").css("border-bottom","none")
         }
+
     })
+
+
+
+
+    }
+   
+
+
+
 }
--- mma/js/index.js
+++ mma/js/index.js
@@ -1,34 +1,51 @@
-// 点击更多进行切换
-$(function () {
-    $("#menu").on("click", ".row>div:nth-child(8)", function () {
-        $("#menu>.row>div:nth-last-child(-n+4)").toggle();
-        return false;
-    })
-    getMenuStyle();
-    getRecommendStyle();
+$(function(){
+    $("#menu").on("click",".row>div:nth-child(8)",function(){
+         $("#menu>.row>div:nth-last-child(-n+4)").toggle();
 
+    })
+
+//获取menu数据
+   getMenuStyle();
+
+   getRecommendStyle()
 
 })
+//menu数据 B
+ function getMenuStyle(){
+        $.ajax({
+            url:url+"api/getindexmenu",
+            success:function(data){
+                // console.log(data)
+                var html = template("indexMenuTpl",data)
+                $("#menu .row").html(html);
+            }
 
-// 获取menu数据
-function getMenuStyle() {
+        })
+ }
+
+//menu数据 E
+
+//  recommend 数据 B
+function getRecommendStyle(){
     $.ajax({
-        url: url + "api/getindexmenu",
-        success: function (data) {
+        url:url+"api/getmoneyctrl",
+        success:function(data){
             // console.log(data)
-            var html = template("menuTpl", data);
-            $("#menu .row").html(html);
+            var html = template("indexRecommendTpl",data)
+            $(".recommend_product").html(html)
         }
+
+
+
     })
 }
 
-function getRecommendStyle() {
-    $.ajax({
-        url: url + "api/getmoneyctrl",
-        success: function (data) {
-            var html = template("recommendTpl", data);
-            $("#recommend .recommend_product").html(html);
-        }
-    })
-}
+
+
+
+
+
+
+//  recommend 数据 E
+
 
